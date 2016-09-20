a86b3be10abd6a8bf3d9322a2282e098d83cf780
ConanXie <xjj416@gmail.com>
2016-09-20 23:51:48 +0800

    search engine data

---
+++ src/modules/Search/search-engine.js
@@ -0,0 +1,13 @@
+export default [{
+  name: 'Google',
+  className: 'google',
+  link: 'https://www.google.com/search?q='
+}, {
+  name: '百度',
+  className: 'baidu',
+  link: 'https://www.baidu.com/s?wd='
+}, {
+  name: 'Bing',
+  className: 'bing',
+  link: 'https://www.bing.com/search?q='
+}]
