b004ee2454af9b5f191da3321e731741fef94fe6
ConanXie <xjj416@gmail.com>
2016-09-22 18:49:31 +0800

    Add a toobar

--- src/modules/Header/Index.jsx
+++ src/modules/Header/Index.jsx
@@ -1,14 +1,30 @@
 import './style.less'
 
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as setupPageActions from '../../actions/setup-page'
 
 import Paper from 'material-ui/Paper'
 import IconButton from 'material-ui/IconButton'
 import NavigationMenu from 'material-ui/svg-icons/navigation/menu'
+import FileFolder from 'material-ui/svg-icons/file/folder'
+import ActionBookmark from 'material-ui/svg-icons/action/bookmark-border'
+import ActionSettings from 'material-ui/svg-icons/action/settings'
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
 
+const style = {
+  headerBar: {
+    borderRadius: 0
+  }
+}
+
 class Header extends Component {
+  static propsType = {
+    showSetup: PropTypes.func.isRequired
+  }
   constructor(props) {
     super(props)
     this.state = {
@@ -21,13 +37,26 @@ class Header extends Component {
     })
   }
   render() {
+    const { showSetup } = this.props
     return (
-      <Paper className="header-bar" zDepth={1}>
-        <IconButton
-          onClick={this.openDrawer}
-        >
-          <NavigationMenu />
-        </IconButton>
+      <Paper className="header-bar" style={style.headerBar} zDepth={1}>
+        <div className="tool-bar">
+          <div className="bar-left">
+            <IconButton
+              onTouchTap={this.openDrawer}
+            >
+              <NavigationMenu />
+            </IconButton>
+            {/*<IconButton>
+              <ActionBookmark />
+            </IconButton>*/}
+          </div>
+          <div className="bar-right">
+            <IconButton onTouchTap={showSetup}>
+              <ActionSettings />
+            </IconButton>
+          </div>
+        </div>
         <Drawer
           docked={false}
           width={305}
@@ -41,4 +70,8 @@ class Header extends Component {
   }
 }
 
-export default Header
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(setupPageActions, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(Header)
