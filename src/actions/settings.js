70dd37bacad10e725631fd2266e9c197a91e6153
ConanXie <xjj416@gmail.com>
2016-09-23 15:17:16 +0800

    Save current Engine

--- src/actions/settings.js
+++ src/actions/settings.js
@@ -1,29 +1,21 @@
 const ls = window.localStorage
 const settings = ls.getItem('settings')
 
-export const GET_SETTINGS = 'GET_SETTINGS'
-
-export function getSettings() {
-  let data
-  try {
-    data = JSON.parse(settings)
-    if (!data) {
-      data = {}
-    }
-  } catch (error) {
-    console.log(error)
-  }
-  console.log(data)
-  return {
-    type: GET_SETTINGS,
-    settings: data
-  }
-}
-
 export function saveSettings(key, value) {
   return (dispatch, getState) => {
     const data = getState().settings.data
     data[key] = value
     ls.setItem('settings', JSON.stringify(data))
+  }
+}
+
+/**
+ * If autoSaveEngine Toggle is true,
+ * save the current engine
+ */
+export function saveCurrentEngine() {
+  return (dispatch, getState) => {
+    const { currentEngine } = getState().searchEngine
+    ls.setItem('currentEngine', JSON.stringify(currentEngine))
   }
 }
