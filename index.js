4728594249d9b5e1ebde4dfa2bdfac874e5b2471
ConanXie <xjj416@gmail.com>
2016-09-28 15:24:20 +0800

    Move the development server to index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack')
+const config = require('./config/webpack.config')
+const compiler = webpack(config)
+
+const app = require('express')()
+const https = require('https')
+const fs = require('fs')
+const port = 5001
+
+const options = {
+  key: fs.readFileSync('./cert/privatekey.pem'),
+  cert: fs.readFileSync('./cert/certificate.pem')
+}
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath,
+  hot: true,
+  stats: {
+    colors: true
+  },
+  inline: true
+}))
+
+app.use(require('webpack-hot-middleware')(compiler))
+
+https.createServer(options, app).listen(port, () => {
+  console.log(`Listening at https://localhost:${port}`)
+})
