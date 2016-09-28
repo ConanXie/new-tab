39aa22bae6977214969d52b370630395f8e651c4
ConanXie <xjj416@gmail.com>
2016-09-28 23:38:38 +0800

    Develop Navigation

--- src/modules/Navigation/Index.jsx
+++ src/modules/Navigation/Index.jsx
@@ -4,22 +4,46 @@ import React, { Component } from 'react'
 
 import Paper from 'material-ui/Paper'
 import FlatButton from 'material-ui/FlatButton'
+import IconButton from 'material-ui/IconButton'
 import ActionAndroid from 'material-ui/svg-icons/action/android'
+import ModeEdit from 'material-ui/svg-icons/editor/mode-edit'
+import ContentAdd from 'material-ui/svg-icons/content/add'
+
+const style = {
+  website: {
+    width: '172px',
+    marginBottom: '8px',
+    textAlign: 'left',
+    color: '#333'
+  }
+}
 
 class Navigation extends Component {
   render() {
     const array = '0'.repeat(10).split('')
     return (
       <Paper zDepth={1} className="navigation-box">
-        <h1 style={{marginBottom: '20px'}}>我的导航</h1>
+        <div className="tool-bar">
+          <h3 className="title">我的导航</h3>
+          <div className="tool-area">
+            <IconButton>
+              <ContentAdd style={{ width: 18, height: 18 }} />
+            </IconButton>
+            <IconButton>
+              <ModeEdit style={{ width: 18, height: 18 }} />
+            </IconButton>
+          </div>
+        </div>
         {array.map(value => {
           return (
             <FlatButton
               key={Math.random()}
-              label="Andriod LINK"
-              href="https://www.android.com/"
+              label="Google"
+              href="https://www.google.com/"
               secondary={true}
-              icon={<img className="website-icon" src="http://c.csdnimg.cn/public/favicon.ico" />}
+              icon={<img className="website-icon" src="https://www.google.com/images/branding/product/ico/googleg_lodp.ico" />}
+              className="website-link"
+              style={style.website}
             />
           )
         })}
@@ -28,8 +52,9 @@ class Navigation extends Component {
             label="Andriod"
             href="https://www.android.com/"
             secondary={true}
-            icon={<img className="website-icon" src="http://c.csdnimg.cn/public/favicon.ico" />}
-            style={{width: '200px', textAlign: 'left'}}
+            icon={<ActionAndroid style={{ width: 18, height: 18 }} />}
+            className="website-link"
+            style={style.website}
           />
       </Paper>
     )
