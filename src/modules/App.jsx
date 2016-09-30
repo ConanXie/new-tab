fb73f439a88c630f00b6a20743484bfad5f02ca0
ConanXie <xjj416@gmail.com>
2016-09-30 18:32:47 +0800

    Change theme color

--- src/modules/App.jsx
+++ src/modules/App.jsx
@@ -3,8 +3,13 @@ import React, { Component } from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import RaisedButton from 'material-ui/RaisedButton'
+import { teal500 } from 'material-ui/styles/colors'
 
-const muiTheme = getMuiTheme(null, {userAgent: 'all'})
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: teal500
+  }
+}, {userAgent: 'all'})
 
 import Header from './Header'
 import Search from './Search'
