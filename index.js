6f3b7e1ad93c1b35d9125ef07aff95f10deccbd8
ConanXie <xjj416@gmail.com>
2016-10-15 17:57:40 +0800

    weather data api

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler))
 
+app.use('/api/weather', (req, res) => {
+  const data = fs.readFileSync('./api/weather.json', 'utf8')
+  res.header('Access-Control-Allow-Origin', '*')
+  res.send(data)
+})
+
 https.createServer(options, app).listen(port, () => {
   console.log(`Listening at https://localhost:${port}`)
 })
