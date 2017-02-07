8903049000a40c1b6eea7ecdce1eada759e1c6b6
ConanXie <xjj416@gmail.com>
2017-02-07 22:47:06 +0800

    Add i18n

--- src/Main.jsx
+++ src/Main.jsx
@@ -13,26 +13,52 @@ import { Provider } from 'react-redux'
 import reducer from './reducers'
 const store = createStore(reducer, applyMiddleware(thunk))
 
+import { IntlProvider } from 'react-intl'
+import zh_CN from './locale/zh_CN'
+import en_US from './locale/en_US'
+
+const lang = navigator.language
+let messages
+switch (lang) {
+  case 'zh-CN':
+    messages = zh_CN
+    break;
+
+  default:
+    messages = en_US
+    break;
+}
+
 import { AppContainer } from 'react-hot-loader'
 import App from './modules/App'
 
 render((
-  <Provider store={store}>
-    <AppContainer>
-      <App />
-    </AppContainer>
-  </Provider>
+  <IntlProvider
+    locale={'en-US'}
+    messages={messages}
+  >
+    <Provider store={store}>
+        <AppContainer>
+          <App />
+        </AppContainer>
+    </Provider>
+  </IntlProvider>
 ), document.querySelector('#app'))
 
 if (module.hot) {
   module.hot.accept('./modules/App', () => {
     const NextApp = require('./modules/App').default
     render((
-      <Provider store={store}>
-        <AppContainer>
-          <NextApp />
-        </AppContainer>
-      </Provider>
+      <IntlProvider
+        locale={'en-US'}
+        messages={messages}
+      >
+        <Provider store={store}>
+          <AppContainer>
+            <NextApp />
+          </AppContainer>
+        </Provider>
+      </IntlProvider>
     ), document.querySelector('#app'))
   })
 }
