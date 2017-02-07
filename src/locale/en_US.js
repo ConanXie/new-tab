dffe304c1a628a3f7c7da9d5b297c989efbd3667
ConanXie <xjj416@gmail.com>
2017-02-07 22:47:20 +0800

    i18n config file

---
+++ src/locale/en_US.js
@@ -0,0 +1,37 @@
+const en_US = {
+  /*general*/
+  'button.cancel': 'CANCEL',
+  'button.confirm': 'OK',
+  /*Nav*/
+  'nav.title': 'Navigation',
+  'nav.edit.title.edit': 'Edit Website',
+  'nav.edit.title.add': 'Add Website',
+  'nav.edit.input.website': 'website name',
+  'nav.edit.input.URL': 'URL',
+  'nav.edit.input.forget.name': 'Don\'t forget website name 🙂',
+  'nav.edit.input.forget.URL': 'Don\'t forget website URL 🙂',
+  'nav.delete.title': 'Confirm the deletion',
+  'nav.delete.tip': 'It is irrevocable after deletion',
+  /*settings*/
+  'settings.toolbar.title': 'Settings',
+  'settings.engine.save.label': 'Automatically save the current search engine',
+  'settings.engine.search.label': 'Open search result in new tab',
+  'settings.website.open.label': 'Open website in new tab',
+  'settings.apps.display.label': 'Hide applications tags',
+  'settings.theme.title': 'Theme',
+  'settings.theme.switch.label': 'Switch color theme',
+  'settings.theme.select.title': 'Select',
+  'settings.br.title': 'Backup and reset',
+  'settings.br.backup.label': 'Create backup file',
+  'settings.br.restore.label': 'Restore from backup',
+  'settings.br.restore.message': 'Success, please refresh window',
+  'settings.br.reset.label': 'Reset to default',
+  'settings.br.reset.message': 'Success, please refresh window',
+  'settings.reset.title': 'Confirm reset settings',
+  'settings.reset.warning': 'It will reset to initialization settings',
+  'settings.about.title': 'About',
+  /*bookmarks*/
+  'bookmarks.search.placeholder': 'search your bookmarks'
+}
+
+export default en_US
---
+++ src/locale/zh_CN.js
@@ -0,0 +1,37 @@
+const zh_CN = {
+  /*general*/
+  'button.cancel': '取消',
+  'button.confirm': '确认',
+  /*Nav*/
+  'nav.title': '我的导航',
+  'nav.edit.title.edit': '编辑网站',
+  'nav.edit.title.add': '增加网站',
+  'nav.edit.input.website': '名称',
+  'nav.edit.input.URL': '链接',
+  'nav.edit.input.forget.name': '别忘了网站的名字🙂',
+  'nav.edit.input.forget.URL': '别忘了网址🙂',
+  'nav.delete.title': '确认删除？',
+  'nav.delete.tip': '删除后不可撤销',
+  /*settings*/
+  'settings.toolbar.title': '设置',
+  'settings.engine.save.label': '自动保存当前搜索引擎',
+  'settings.engine.search.label': '在新标签页中打开搜索',
+  'settings.website.open.label': '在新标签页中打开网站',
+  'settings.apps.display.label': '隐藏应用的标签',
+  'settings.theme.title': '主题',
+  'settings.theme.switch.label': '切换主题',
+  'settings.theme.select.title': '选择主题',
+  'settings.br.title': '备份与重置',
+  'settings.br.backup.label': '生成备份文件',
+  'settings.br.restore.label': '从备份中恢复',
+  'settings.br.restore.message': '已从备份文件恢复, 刷新后生效',
+  'settings.br.reset.label': '重置为默认设置',
+  'settings.br.reset.message': '已重置设置，刷新后生效',
+  'settings.reset.title': '确认重置设置？',
+  'settings.reset.warning': '将会重置为初始化设置',
+  'settings.about.title': '关于',
+  /*bookmarks*/
+  'bookmarks.search.placeholder': '搜索书签'
+}
+
+export default zh_CN
