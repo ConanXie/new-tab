25d5ebe74327a162652081b10745fa2d07defd3d
ConanXie <xjj416@gmail.com>
2017-02-20 23:40:45 +0800

    bookmarks tree

--- src/modules/Header/Bookmark/Index.jsx
+++ src/modules/Header/Bookmark/Index.jsx
@@ -4,12 +4,15 @@ import classNames from 'classnames'
 import React, { Component, PropTypes } from 'react'
 import { findDOMNode } from 'react-dom'
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Paper from 'material-ui/Paper'
 import { List, ListItem } from 'material-ui/List'
 import IconButton from 'material-ui/IconButton'
 import ActionSearch from 'material-ui/svg-icons/action/search'
 import ContentClear from 'material-ui/svg-icons/content/clear'
 import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back'
+import FileFolder from 'material-ui/svg-icons/file/folder'
+import FolderOpen from 'material-ui/svg-icons/file/folder-open'
 
 const style = {
   listItem: {
@@ -120,6 +123,128 @@ class Search extends Component {
   }
 }
 
+// distance
+const padding = 16
+
+class Folder extends Component {
+  static propsType = {
+    // data: PropTypes.object.isRequired,
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    isOpen: PropTypes.bool.isRequired,
+    level: PropTypes.number.isRequired
+  }
+  constructor(props) {
+    super(props)
+    this.state = {
+      folderStatus: 'closed',
+      generated: false,
+      ChildrenLists: ''
+    }
+  }
+  componentWillMount() {
+    /*chrome.bookmark.get(this.props.id, d => {
+      this.setState({
+        title: d.title
+      })
+    })*/
+    const { isOpen } = this.props
+    if (isOpen) {
+      this.createTree()
+    }
+  }
+  createTree = () => {
+    const { id, level } = this.props
+    chrome.bookmarks.getChildren(id, children => {
+      this.count = children.length
+      
+      const ChildrenLists = this.generateChildren(children, level + 1)
+      
+      this.setState({
+        folderStatus: 'opened',
+        generated: true,
+        ChildrenLists
+      })
+    })
+  }
+  generateChildren = (data, level) => {
+    const paddingLeft = padding * level
+
+    const Lists = data.map(i => {
+      const children = i.children
+      const title = i.title
+      const url = i.url
+      const id = i.id
+      const parentId = i.parentId
+      const isFolder = i.dateGroupModified || children || typeof url == 'undefined'
+
+      if (isFolder) {
+        return <Folder key={id} id={id} title={title} isOpen={false} level={level} />
+      } else {
+        return (
+          <a className="bookmark-link" href={url} key={id} title={title}>
+            <ListItem
+              primaryText={title}
+              className="bookmark"
+              innerDivStyle={style.bookmark}
+              style={{ paddingLeft }}
+              leftIcon={<div style={{ width: 16, height: 16, margin: 8, content: `-webkit-image-set(url("chrome://favicon/size/16@1x/${url}") 1x, url("chrome://favicon/size/16@2x/${url}") 2x)` }}></div>}
+            />
+          </a>
+        )
+      }
+    })
+    return Lists
+  }
+  changeStatus = () => {
+    const { folderStatus, generated } = this.state
+    
+    if (!generated) {
+      this.createTree()
+    } else {
+      this.setState({
+        folderStatus: folderStatus === 'closed' ? 'opened' : 'closed'
+      })
+    }
+    /* else if (folderStatus === 'closed') {
+      this.refs.children.style.display = 'block'
+      this.setState({
+        folderStatus: 'opened'
+      })
+    } else {
+      this.refs.children.style.display = 'none'
+      this.setState({
+        folderStatus: 'closed'
+      })
+    }*/
+  }
+  render() {
+    const { title, isOpen, level } = this.props
+    const { folderStatus, ChildrenLists } = this.state
+
+    let Icon = <FileFolder style={{ margin: 4 }} />
+    if (folderStatus === 'opened') {
+      Icon = <FolderOpen style={{ margin: 4 }} />
+    }
+
+    return (
+      <div>
+        <ListItem
+          primaryText={title}
+          className="folder"
+          innerDivStyle={style.bookmark}
+          style={{ paddingLeft: level * padding }}
+          leftIcon={Icon}
+          onTouchTap={e => { this.changeStatus() }}
+        />
+        <div className={classNames('children-wrap', { 'opened': folderStatus === 'opened' })}>
+          {ChildrenLists}
+        </div>
+      </div>
+    )
+  }
+}
+
 class Bookmark extends Component {
   static contextTypes = {
     intl: PropTypes.object.isRequired
@@ -129,15 +254,127 @@ class Bookmark extends Component {
     this.state = {
       search: false,
       bookmarks: [],
-      currentChildren: []
+      currentChildren: [],
+      opens: ['1']
     }
   }
-  componentWillMount() {
+  generateBookmarks = (data, level = 0) => {
+    // const generate = (data, level = 0) => {
+      // const paddingLeft = 34 * level
+      const paddingLeft = 16 * level
+      const group = (level == 0) ? 'tree' : 'group'
+      // const Wrap = <List>{this.props.children}</List>
+      /*for (let i = 0; i < data.length; i++) {
+        const d = data[i]
+        const children = d.children
+        const title = d.title.htmlspecialchars()
+        const url = d.url
+        const id = d.id
+        const parentId = d.parentId
+        // const idHTML = id ? 
+        const isFolder = d.dateGroupModified || children || typeof url == 'undefined'
+        let Item
+        if (isFolder) {
+          Item = <ListItem>{title}</ListItem>
+        } else {
+          Item = <ListItem>{title}</ListItem>
+        }
+      }*/
+      let Items
+      if (data.length) {
+        Items = data.map(i => {
+          const children = i.children
+          const title = i.title
+          const url = i.url
+          const id = i.id
+          const parentId = i.parentId
+          const isFolder = i.dateGroupModified || children || typeof url == 'undefined'
+          if (isFolder) {
+            const isOpen = this.state.opens.indexOf(id) != -1
+            // const isOpen = true
+            /*if (isOpen) {
+              return (
+                <div key={id}>
+                  <div>
+                    <ListItem
+                      primaryText={title}
+                      className="folder"
+                      innerDivStyle={style.bookmark}
+                      style={{ paddingLeft }}
+                      leftIcon={<FolderOpen style={{ margin: 4 }} />}
+                      onTouchTap={e => { this.folderStatus(e, id, level + 1) }}
+                    />
+                    <div className="children-wrap">
+                      {this.generateBookmarks(children, level + 1)}
+                    </div>
+                  </div>
+                </div>
+              )
+            } else {
+              return (
+                <div key={id}>
+                  <div>
+                    <ListItem
+                      key={id}
+                      primaryText={title}
+                      className="folder"
+                      innerDivStyle={style.bookmark}
+                      style={{ paddingLeft }}
+                      leftIcon={<FileFolder style={{ margin: 4 }} />}
+                      onTouchTap={e => { this.folderStatus(e, id, level + 1) }}
+                    />
+                    <div className="children-wrap"></div>
+                  </div>
+                </div>
+              )
+            }*/
+            return <Folder key={id} id={id} title={title} isOpen={isOpen} level={level} />
+          } else {
+            return (
+              <ListItem
+                key={id}
+                primaryText={title}
+                className="bookmark"
+                innerDivStyle={style.bookmark}
+                style={{ paddingLeft }}
+                leftIcon={<div style={{ width: 16, height: 16, margin: 8, content: `-webkit-image-set(url("chrome://favicon/size/16@1x/${url}") 1x, url("chrome://favicon/size/16@2x/${url}") 2x)` }}></div>}
+              />
+            )
+          }
+        })
+      }
+      return <List key={data.id}>{Items}</List>
+    // }
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      chrome.bookmarks.getTree(tree => {
+        const { opens } = this.state
+        const data = tree[0].children
+        let Lists
+        if (data) {
+          Lists = data.map(i => {
+            const title = i.title
+            const id = i.id
+            const level = 0
+            const isOpen = opens.indexOf(id) != -1
+            // const isOpen = true
+            
+            return (
+              <Folder key={id} id={id} title={title} isOpen={isOpen} level={level} />
+            )
+          })
+        }
+        this.setState({
+          bookmarks: Lists
+        })
+      })
+    }, 1000)
     /**
      * create bookmarks data
      */
-    chrome.bookmarks.getTree((bookmarks) => {
-      // console.log(bookmarks)
+    /*chrome.bookmarks.getTree((bookmarks) => {
+      console.log(bookmarks)
       const data = []
       const iteration = function (value, index) {
         if (value.children) {
@@ -171,14 +408,52 @@ class Bookmark extends Component {
         bookmarks: data,
         currentChildren: data[0].children
       })
-    })
-    
+    })*/
+    // this.generateBookmarks()
   }
-  getChildren = (index) => {
+  /*folderStatus = (e, id, level) => {
+    // chrome.bookmarks.get('1', v => { console.log(v) })
+    const target = e.currentTarget
+    const index = this.state.opens.indexOf(id)
+    // open folder
+    if (index === -1) {
+      this.state.opens.push(id)
+      const parentNode = target.parentNode.parentNode
+      console.log(parentNode)
+      chrome.bookmarks.getChildren(id, children => {
+        const ChildrenLists = this.generateBookmarks(children, level)
+        chrome.bookmarks.get(id, d => {
+          console.log(d)
+          const title = d[0].title
+          const paddingLeft = (level - 1) * 16
+          render((
+            <MuiThemeProvider muiTheme={this.props.muiTheme}>
+              <div>
+                <ListItem
+                  primaryText={title}
+                  className="folder"
+                  innerDivStyle={style.bookmark}
+                  style={{ paddingLeft }}
+                  leftIcon={<FolderOpen style={{ margin: 4 }} />}
+                  onTouchTap={e => { this.folderStatus(e, id, level + 1) }}
+                />
+                <div className="children-wrap">{ChildrenLists}</div>
+              </div>
+            </MuiThemeProvider>
+          ), parentNode)
+        })
+      })
+    // close folder
+    } else {
+      this.state.opens.splice(index, 1)
+    }
+    // this.generateBookmarks()
+  }*/
+  /*getChildren = (index) => {
     this.setState({
       currentChildren: this.state.bookmarks[index].children
     })
-  }
+  }*/
   openSearch = () => {
     this.setState({
       search: true
@@ -194,15 +469,20 @@ class Bookmark extends Component {
     const { intl } = this.context
     return (
       <div className="bookmark-component">
-        <header style={{ backgroundColor: muiTheme.palette.primary1Color }}>
-          <div className="search-box" onTouchTap={this.openSearch}>
-            <div className="search-icon">
-              <ActionSearch color="#fff" />
+        <Paper zDepth={1}>
+          <header style={{ backgroundColor: muiTheme.palette.primary1Color }}>
+            <div className="search-box" onTouchTap={this.openSearch}>
+              <div className="search-icon">
+                <ActionSearch color="#fff" />
+              </div>
+              <div className="placeholder">{intl.formatMessage({ id: 'bookmarks.search.placeholder' })}</div>
             </div>
-            <div className="placeholder">{intl.formatMessage({ id: 'bookmarks.search.placeholder' })}</div>
-          </div>
-        </header>
-        <aside className="folder-list">
+          </header>
+        </Paper>
+        <section className="folder-list">
+          {this.state.bookmarks}
+        </section>
+        {/*<aside className="folder-list">
           <List>
             {this.state.bookmarks.map((value, index) => {
               return (
@@ -232,7 +512,7 @@ class Bookmark extends Component {
               )
             })}
           </List>
-        </section>
+        </section>*/}
         <Search open={this.state.search} muiTheme={muiTheme} close={this.closeSearch} />
       </div>
     )
