f19ee0b31e8856599d2adb55eca468825d345a49
ConanXie <xjj416@gmail.com>
2017-03-13 00:00:05 +0800

    Optimize code

--- src/modules/Header/Bookmark/Index.jsx
+++ src/modules/Header/Bookmark/Index.jsx
@@ -136,11 +136,12 @@ const padding = 16
 
 class Folder extends Component {
   static propsType = {
-    // data: PropTypes.object.isRequired,
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     isOpen: PropTypes.bool.isRequired,
-    level: PropTypes.number.isRequired
+    level: PropTypes.number.isRequired,
+    opens: PropTypes.array.isRequired,
+    recordFolders: PropTypes.func.isRequired
   }
   constructor(props) {
     super(props)
@@ -151,11 +152,6 @@ class Folder extends Component {
     }
   }
   componentWillMount() {
-    /*chrome.bookmark.get(this.props.id, d => {
-      this.setState({
-        title: d.title
-      })
-    })*/
     const { isOpen } = this.props
     if (isOpen) {
       this.createTree()
@@ -190,7 +186,17 @@ class Folder extends Component {
 
       if (isFolder) {
         const isOpen = opens.indexOf(id) != -1
-        return <Folder key={id} id={id} title={title} level={level} isOpen={isOpen} opens={opens} recordFolders={recordFolders} />
+        return (
+          <Folder
+            key={id}
+            id={id}
+            title={title}
+            level={level}
+            isOpen={isOpen}
+            opens={opens}
+            recordFolders={recordFolders}
+          />
+        )
       } else {
         return (
           <a className="bookmark-link" href={url} key={id} title={title}>
@@ -218,17 +224,6 @@ class Folder extends Component {
       })
     }
     recordFolders(id)
-    /* else if (folderStatus === 'closed') {
-      this.refs.children.style.display = 'block'
-      this.setState({
-        folderStatus: 'opened'
-      })
-    } else {
-      this.refs.children.style.display = 'none'
-      this.setState({
-        folderStatus: 'closed'
-      })
-    }*/
   }
   render() {
     const { title, isOpen, level } = this.props
@@ -271,104 +266,19 @@ class Bookmark extends Component {
     this.opens = []
   }
   componentWillReceiveProps(nextProps, nextState) {
-    console.log(nextProps)
     const { rememberBookmarksState } = nextProps
+
+    // if cancel remember state then set scrollTop and opens to default
     if (!rememberBookmarksState) {
       localStorage.setItem('bookmarksScrollTop', 0)
       localStorage.setItem('bookmarksOpens', '[]')
     } else {
+      // save current state to localStorage
       localStorage.setItem('bookmarksScrollTop', this.refs.wrapper.scrollTop)
       localStorage.setItem('bookmarksOpens', JSON.stringify(this.opens))
     }
   }
-  generateBookmarks = (data, level = 0) => {
-    // const generate = (data, level = 0) => {
-      // const paddingLeft = 34 * level
-      const paddingLeft = 16 * level
-      const group = (level == 0) ? 'tree' : 'group'
-      // const Wrap = <List>{this.props.children}</List>
-      /*for (let i = 0; i < data.length; i++) {
-        const d = data[i]
-        const children = d.children
-        const title = d.title.htmlspecialchars()
-        const url = d.url
-        const id = d.id
-        const parentId = d.parentId
-        // const idHTML = id ? 
-        const isFolder = d.dateGroupModified || children || typeof url == 'undefined'
-        let Item
-        if (isFolder) {
-          Item = <ListItem>{title}</ListItem>
-        } else {
-          Item = <ListItem>{title}</ListItem>
-        }
-      }*/
-      let Items
-      if (data.length) {
-        Items = data.map(i => {
-          const children = i.children
-          const title = i.title
-          const url = i.url
-          const id = i.id
-          const parentId = i.parentId
-          const isFolder = i.dateGroupModified || children || typeof url == 'undefined'
-          if (isFolder) {
-            const isOpen = this.state.opens.indexOf(id) != -1
-            // const isOpen = true
-            /*if (isOpen) {
-              return (
-                <div key={id}>
-                  <div>
-                    <ListItem
-                      primaryText={title}
-                      className="folder"
-                      innerDivStyle={style.bookmark}
-                      style={{ paddingLeft }}
-                      leftIcon={<FolderOpen style={{ margin: 4 }} />}
-                      onTouchTap={e => { this.folderStatus(e, id, level + 1) }}
-                    />
-                    <div className="children-wrap">
-                      {this.generateBookmarks(children, level + 1)}
-                    </div>
-                  </div>
-                </div>
-              )
-            } else {
-              return (
-                <div key={id}>
-                  <div>
-                    <ListItem
-                      key={id}
-                      primaryText={title}
-                      className="folder"
-                      innerDivStyle={style.bookmark}
-                      style={{ paddingLeft }}
-                      leftIcon={<FileFolder style={{ margin: 4 }} />}
-                      onTouchTap={e => { this.folderStatus(e, id, level + 1) }}
-                    />
-                    <div className="children-wrap"></div>
-                  </div>
-                </div>
-              )
-            }*/
-            return <Folder key={id} id={id} title={title} isOpen={isOpen} level={level} />
-          } else {
-            return (
-              <ListItem
-                key={id}
-                primaryText={title}
-                className="bookmark"
-                innerDivStyle={style.bookmark}
-                style={{ paddingLeft }}
-                leftIcon={<div style={{ width: 16, height: 16, margin: 8, content: `-webkit-image-set(url("chrome://favicon/size/16@1x/${url}") 1x, url("chrome://favicon/size/16@2x/${url}") 2x)` }}></div>}
-              />
-            )
-          }
-        })
-      }
-      return <List key={data.id}>{Items}</List>
-    // }
-  }
+  // load bookmarks
   componentDidMount() {
 
     // get data from localStorage
@@ -379,24 +289,28 @@ class Bookmark extends Component {
 
     setTimeout(() => {
       chrome.bookmarks.getTree(tree => {
-        // const { opens } = this.state
         const data = tree[0].children
         let bookmarks
         if (data) {
-          const result = data.map(i => {
+          const results = data.map(i => {
             const title = i.title
             const id = i.id
-            console.log(id)
             const level = 0
-            console.log(this)
             const isOpen = this.opens.indexOf(id) !== -1
-            // const isOpen = true
             
             return (
-              <Folder key={id} id={id} title={title} level={level} isOpen={isOpen} opens={this.opens} recordFolders={this.recordFolders} />
+              <Folder
+                key={id}
+                id={id}
+                title={title}
+                level={level}
+                isOpen={isOpen}
+                opens={this.opens}
+                recordFolders={this.recordFolders}
+              />
             )
           })
-          bookmarks = <List>{result}</List>
+          bookmarks = <List>{results}</List>
         }
         this.setState({
           bookmarks
@@ -406,46 +320,6 @@ class Bookmark extends Component {
         }, 200)
       })
     }, 500)
-    /**
-     * create bookmarks data
-     */
-    /*chrome.bookmarks.getTree((bookmarks) => {
-      console.log(bookmarks)
-      const data = []
-      const iteration = function (value, index) {
-        if (value.children) {
-          const _index = data.length
-          data[_index] = {
-            name: value.title,
-            children: []
-          }
-          value.children.forEach((v, i) => {
-            if (v.children) {
-              const v_index = data.length
-              data[v_index] = {
-                name: v.title,
-                children: []
-              }
-              v.children.forEach((m, n) => {
-                iteration(m, v_index)
-              })
-            } else {
-              data[_index].children.push(v)
-            }
-          })
-        } else {
-          data[index].children.push(value)
-        }
-      }
-      bookmarks[0].children.forEach((value, index) => {
-        iteration(value)
-      })
-      this.setState({
-        bookmarks: data,
-        currentChildren: data[0].children
-      })
-    })*/
-    // this.generateBookmarks()
   }
   listenScroll = (event) => {
     // if user need to remember bookmarks state
@@ -464,49 +338,6 @@ class Bookmark extends Component {
       localStorage.setItem('bookmarksOpens', JSON.stringify(this.opens))
     }
   }
-  /*folderStatus = (e, id, level) => {
-    // chrome.bookmarks.get('1', v => { console.log(v) })
-    const target = e.currentTarget
-    const index = this.state.opens.indexOf(id)
-    // open folder
-    if (index === -1) {
-      this.state.opens.push(id)
-      const parentNode = target.parentNode.parentNode
-      console.log(parentNode)
-      chrome.bookmarks.getChildren(id, children => {
-        const ChildrenLists = this.generateBookmarks(children, level)
-        chrome.bookmarks.get(id, d => {
-          console.log(d)
-          const title = d[0].title
-          const paddingLeft = (level - 1) * 16
-          render((
-            <MuiThemeProvider muiTheme={this.props.muiTheme}>
-              <div>
-                <ListItem
-                  primaryText={title}
-                  className="folder"
-                  innerDivStyle={style.bookmark}
-                  style={{ paddingLeft }}
-                  leftIcon={<FolderOpen style={{ margin: 4 }} />}
-                  onTouchTap={e => { this.folderStatus(e, id, level + 1) }}
-                />
-                <div className="children-wrap">{ChildrenLists}</div>
-              </div>
-            </MuiThemeProvider>
-          ), parentNode)
-        })
-      })
-    // close folder
-    } else {
-      this.state.opens.splice(index, 1)
-    }
-    // this.generateBookmarks()
-  }*/
-  /*getChildren = (index) => {
-    this.setState({
-      currentChildren: this.state.bookmarks[index].children
-    })
-  }*/
   openSearch = () => {
     this.setState({
       search: true
@@ -539,37 +370,6 @@ class Bookmark extends Component {
           )}
           {bookmarks}
         </section>
-        {/*<aside className="folder-list">
-          <List>
-            {this.state.bookmarks.map((value, index) => {
-              return (
-                <ListItem
-                  primaryText={value.name}
-                  onTouchTap={e => { this.getChildren(index) }}
-                  key={index}
-                  innerDivStyle={style.listItem}
-                  className="folder"
-                />
-              )
-            })}
-          </List>
-        </aside>
-        <section className="bookmarks-sec">
-          <List>
-            {this.state.currentChildren.map((value, index) => {
-              return (
-                <a className="bookmark-link" href={value.url} key={index} title={value.title}>
-                  <ListItem
-                    primaryText={value.title}
-                    leftIcon={<div style={{ width: 16, height: 16, margin: 8, content: `-webkit-image-set(url("chrome://favicon/size/16@1x/${value.url}") 1x, url("chrome://favicon/size/16@2x/${value.url}") 2x)` }}></div>}
-                    className="bookmark"
-                    innerDivStyle={style.bookmark}
-                  />
-                </a>
-              )
-            })}
-          </List>
-        </section>*/}
         <Search open={this.state.search} muiTheme={muiTheme} close={this.closeSearch} />
       </div>
     )
