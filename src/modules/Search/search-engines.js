084cddda3cf530816b4057107aaf506a82df3b76
ConanXie <xjj416@gmail.com>
2017-04-28 18:21:58 +0800

    Add isDefault field in default engines list

--- src/modules/Search/search-engines.js
+++ src/modules/Search/search-engines.js
@@ -2,20 +2,24 @@ export default [{
   name: 'Google',
   className: 'google',
   link: 'https://www.google.com/search?q=%s',
-  predict: 'https://www.google.com/complete/search?client=serp&hl=%l&q=%s&xhr=t'
+  predict: 'https://www.google.com/complete/search?client=serp&hl=%l&q=%s&xhr=t',
+  isDefault: true
 }, {
   name: '百度',
   className: 'baidu',
   link: 'https://www.baidu.com/s?wd=%s',
-  predict: 'https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd=%s&json=1'
+  predict: 'https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd=%s&json=1',
+  isDefault: false
 }, {
   name: '搜狗',
   className: 'sogou',
   link: 'https://www.sogou.com/web?query=%s',
-  predict: 'https://www.sogou.com/suggnew/ajajjson?key=%s&type=web'
+  predict: 'https://www.sogou.com/suggnew/ajajjson?key=%s&type=web',
+  isDefault: false
 }, {
   name: 'Bing',
   className: 'bing',
   link: 'https://www.bing.com/search?q=%s',
-  predict: 'https://www.google.com/complete/search?client=serp&hl=%l&q=%s&xhr=t'
+  predict: 'https://www.google.com/complete/search?client=serp&hl=%l&q=%s&xhr=t',
+  isDefault: false
 }]
