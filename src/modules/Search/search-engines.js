fda1791f47cc8a402874d992774b437d8ba1ab80
ConanXie <xjj416@gmail.com>
2017-05-02 18:35:28 +0800

    Remove isDefault field

--- src/modules/Search/search-engines.js
+++ src/modules/Search/search-engines.js
@@ -2,24 +2,20 @@ export default [{
   name: 'Google',
   className: 'google',
   link: 'https://www.google.com/search?q=%s',
-  predict: 'https://www.google.com/complete/search?client=serp&hl=%l&q=%s&xhr=t',
-  isDefault: true
+  predict: 'https://www.google.com/complete/search?client=serp&hl=%l&q=%s&xhr=t'
 }, {
   name: '百度',
   className: 'baidu',
   link: 'https://www.baidu.com/s?wd=%s',
-  predict: 'https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd=%s&json=1',
-  isDefault: false
+  predict: 'https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd=%s&json=1'
 }, {
   name: '搜狗',
   className: 'sogou',
   link: 'https://www.sogou.com/web?query=%s',
-  predict: 'https://www.sogou.com/suggnew/ajajjson?key=%s&type=web',
-  isDefault: false
+  predict: 'https://www.sogou.com/suggnew/ajajjson?key=%s&type=web'
 }, {
   name: 'Bing',
   className: 'bing',
   link: 'https://www.bing.com/search?q=%s',
-  predict: 'https://www.google.com/complete/search?client=serp&hl=%l&q=%s&xhr=t',
-  isDefault: false
+  predict: 'https://www.google.com/complete/search?client=serp&hl=%l&q=%s&xhr=t'
 }]
