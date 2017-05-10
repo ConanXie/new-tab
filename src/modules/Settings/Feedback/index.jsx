769a63d73b1354111ecd5b894b4fc75a7e9d35c1
ConanXie <xjj416@gmail.com>
2017-05-10 18:31:52 +0800

    Todo: feedback

--- src/modules/Settings/Feedback/index.jsx
+++ src/modules/Settings/Feedback/index.jsx
@@ -1,6 +1,7 @@
 import './style.less'
 
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 import muiThemeable from 'material-ui/styles/muiThemeable'
 import FlatButton from 'material-ui/FlatButton'
@@ -24,16 +25,21 @@ const style = {
 }
 
 class Feedback extends Component {
+  static contextTypes = {
+    intl: PropTypes.object.isRequired
+  }
   constructor(props) {
     super(props)
     this.state = {
+      email: '',
+      content: '',
       dialogOpen: false,
       snackbarOpen: false,
       snackbarMessage: ''
     }
     this.emailPattern = /^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/
-    // this.feedbackUrl = 'http://localhost:5300/api/feedback'
-    this.feedbackUrl = 'https://tab.xiejie.co/api/feedback'
+    this.feedbackUrl = 'http://localhost:5300/api/feedback'
+    // this.feedbackUrl = 'https://tab.xiejie.co/api/feedback'
     this.loading = false
   }
   openDialog = () => {
@@ -70,21 +76,23 @@ class Feedback extends Component {
    * submit feedback
    */
   handleSubmit = () => {
+    const { intl } = this.context
     // prevent multipul submit
     if (this.loading) return
 
     const { email, content } = this.state
-    if (!this.emailPattern.test(email)) {
+    
+    if (email && !this.emailPattern.test(email)) {
       this.setState({
         snackbarOpen: true,
-        snackbarMessage: '电子邮件格式不正确🙂'
+        snackbarMessage: intl.formatMessage({ id: 'feedback.email.error' })
       })
       return
     }
     if (!content) {
       this.setState({
         snackbarOpen: true,
-        snackbarMessage: '说点什么吧🙂'
+        snackbarMessage: intl.formatMessage({ id: 'feedback.message.error' })
       })
       return
     }
@@ -100,7 +108,7 @@ class Feedback extends Component {
         res.json().then(data => {
           this.setState({
             snackbarOpen: true,
-            snackbarMessage: data.code ? data.msg : '反馈成功，十分感谢'
+            snackbarMessage: data.code ? data.msg : 'I received your feedback, thank you.'
           })
           setTimeout(() => {
             this.loading = false
@@ -113,23 +121,24 @@ class Feedback extends Component {
   render() {
     const { dialogOpen, snackbarOpen, snackbarMessage } = this.state
     const { muiTheme } = this.props
+    const { intl } = this.context
     const actions = [
       <FlatButton
-        label="取消"
+        label={intl.formatMessage({ id: 'button.cancel' })}
         primary={true}
         onTouchTap={this.hideDialog}
       />,
       <FlatButton
-        label="确认"
+        label={intl.formatMessage({ id: 'button.confirm' })}
         primary={true}
         onTouchTap={this.handleSubmit}
       />
     ]
     return (
       <div className="feedback">
-        <FlatButton label="反馈" onTouchTap={this.openDialog} />
+        <FlatButton label={intl.formatMessage({ id: 'settings.feedback' })} onTouchTap={this.openDialog} />
         <Dialog
-          title="反馈"
+          title={intl.formatMessage({ id: 'settings.feedback' })}
           actions={actions}
           modal={false}
           open={dialogOpen}
@@ -138,13 +147,13 @@ class Feedback extends Component {
           titleStyle={style.dialogTitle}
         >
           <TextField
-            floatingLabelText="你的电子邮件地址"
+            floatingLabelText={intl.formatMessage({ id: 'feedback.email.placeholder' })}
             style={style.textField}
             onChange={this.emailChange}
           /><br />
           <TextField
             multiLine={true}
-            floatingLabelText="有什么想对我说的"
+            floatingLabelText={intl.formatMessage({ id: 'feedback.message.placeholder' })}
             rows={2}
             rowsMax={4}
             style={style.textField}
