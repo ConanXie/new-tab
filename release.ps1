75aaf9ceed3f25c936329c304151ac5c4b065604
ConanXie <xjj416@gmail.com>
2017-05-14 14:07:24 +0800

    Fix release script

--- release.ps1
+++ release.ps1
@@ -24,9 +24,9 @@ Set-Location "dist"
 # manifest.json
 $manifest = Get-Content manifest.json
 $manifest = $manifest -replace "(\d+\.){2}\d+", $args -replace "\]\,", "]"
-# $manifest = $manifest[0..18] + $manifest[20]
+# $manifest = $manifest[0..20] + $manifest[22]
 [System.Collections.ArrayList]$manifest = $manifest
-$manifest.RemoveAt(19)
+$manifest.RemoveAt(20)
 Set-Content manifest.json -Value $manifest
 
 # index.html
