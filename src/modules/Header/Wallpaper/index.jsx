315c9d8993b86d185813953313ee5ce95e54dd47
ConanXie <xjj416@gmail.com>
2017-05-26 23:43:08 +0800

    Todo: Wallpaper

---
+++ src/modules/Header/Wallpaper/index.jsx
@@ -0,0 +1,16 @@
+import './style.less'
+
+import React, { Component } from 'react'
+
+class Wallpaper extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <h1>Wallpaper</h1>
+    )
+  }
+}
+
+export default Wallpaper
