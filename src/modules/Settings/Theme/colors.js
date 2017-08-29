dbf7c34b30a5ae6fcac640dd084c15ffeec200fc
ConanXie <xjj416@gmail.com>
2017-08-29 18:52:55 +0800

    Apart colors from Theme Component

---
+++ src/modules/Settings/Theme/colors.js
@@ -0,0 +1,67 @@
+import {
+  grey600,
+  teal500,
+  red500,
+  pink500,
+  indigo500,
+  blue500,
+  deepOrange500,
+  purple500,
+  green500,
+  orange500,
+  blueGrey500,
+  amber500,
+  brown500,
+  grey500,
+  lightBlue500,
+  deepPurple500
+} from 'material-ui/styles/colors'
+
+const colors = [{
+  name: chrome.i18n.getMessage('themeTeal'),
+  color: teal500
+}, {
+  name: chrome.i18n.getMessage('themeRed'),
+  color: red500
+}, {
+  name: chrome.i18n.getMessage('themePink'),
+  color: pink500
+}, {
+  name: chrome.i18n.getMessage('themeIndigo'),
+  color: indigo500
+}, {
+  name: chrome.i18n.getMessage('themeBlue'),
+  color: blue500
+}, {
+  name: chrome.i18n.getMessage('themeDeepOrange'),
+  color: deepOrange500
+}, {
+  name: chrome.i18n.getMessage('themePurple'),
+  color: purple500
+}, {
+  name: chrome.i18n.getMessage('themeGreen'),
+  color: green500
+}, {
+  name: chrome.i18n.getMessage('themeOrange'),
+  color: orange500
+}, {
+  name: chrome.i18n.getMessage('themeBlueGrey'),
+  color: blueGrey500
+}, {
+  name: chrome.i18n.getMessage('themeAmber'),
+  color: amber500
+}, {
+  name: chrome.i18n.getMessage('themeBrown'),
+  color: brown500
+}, {
+  name: chrome.i18n.getMessage('themeGrey'),
+  color: grey500
+}, {
+  name: chrome.i18n.getMessage('themeLightBlue'),
+  color: lightBlue500
+}, {
+  name: chrome.i18n.getMessage('themeDeepPurple'),
+  color: deepPurple500
+}]
+
+export default colors
