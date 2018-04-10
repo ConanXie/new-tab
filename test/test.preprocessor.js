fd1eb1516b711524df5fa30aae254be9f28f25ee
ConanXie <xjj416@gmail.com>
2018-04-10 22:38:34 +0800

    Some configs for testing

---
+++ test/test.preprocessor.js
@@ -0,0 +1,21 @@
+/**
+ * Transpiles TypeScript to JavaScript code.
+ *
+ * @link https://github.com/facebook/jest/blob/master/examples/typescript/preprocessor.js
+ * @copyright 2004-present Facebook. All Rights Reserved.
+ */
+
+const tsc = require('typescript')
+const tsConfig = require('../tsconfig.json')
+
+// Use CommonJS module
+tsConfig.compilerOptions.module = 'commonjs'
+
+module.exports = {
+  process(src, path) {
+    if (path.endsWith('.ts') || path.endsWith('.tsx')) {
+      return tsc.transpile(src, tsConfig.compilerOptions, path, [])
+    }
+    return src
+  }
+}
