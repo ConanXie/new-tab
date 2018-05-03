dbe375a2e42c66babc64728fbadf3e86cfc5e039
ConanXie <xjj416@gmail.com>
2018-05-03 22:37:44 +0800

    Create theme auto by observing theme color

--- src/views/settings/store/Settings.ts
+++ src/views/settings/store/Settings.ts
@@ -1,11 +1,23 @@
-import { observable } from "mobx"
+import { observable, computed } from "mobx"
+import { createMuiTheme } from "material-ui"
 
 const themeColor = localStorage.getItem("theme") || "#3F51B5"
 
 export class Settings {
   @observable public themeColor = themeColor
-  public saveThemeColor(color: string) {
-    this.themeColor = color
+  @computed get theme() {
+    const color = this.themeColor
+    localStorage.setItem("theme", color)
+    return Settings.createTheme(color)
+  }
+  private static createTheme(color: string) {
+    return createMuiTheme({
+      palette: {
+        primary: {
+          main: color
+        }
+      }
+    })
   }
 }
 
