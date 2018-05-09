cca03b1c23bdf7f3f1b94d2c292abda9ac0e00cd
ConanXie <xjj416@gmail.com>
2018-05-09 22:53:19 +0800

    Code to beautify

--- src/views/index/components/Header/index.tsx
+++ src/views/index/components/Header/index.tsx
@@ -10,23 +10,11 @@ import Toolbar from "./Toolbar"
 import "./style"
 
 type StylesType = "root"
-  | "gutters"
-  | "iconLight"
-  | "iconDark"
 
 const styles: StyleRulesCallback<StylesType> = theme => ({
   root: {
     backgroundColor: "transparent",
     boxShadow: "none"
-  },
-  gutters: {
-    justifyContent: "flex-end"
-  },
-  iconLight: {
-    color: theme.palette.grey["50"]
-  },
-  iconDark: {
-    color: theme.palette.grey["800"]
   }
 })
 
@@ -46,12 +34,11 @@ class Header extends React.Component<WithStyles<StylesType>> {
   }
   public render() {
     const { classes } = this.props
-    // const className = wallpaperStore.darkIcons ? classes.iconDark : classes.iconLight
 
     return (
-      <div>
+      <React.Fragment>
         <AppBar square className={classes.root}>
-        <Toolbar onWallpaperIconClick={this.openWallpaperDrawer} />
+          <Toolbar onWallpaperIconClick={this.openWallpaperDrawer} />
         </AppBar>
         <Drawer anchor="right" open={this.state.wallpaperOpen} onClose={this.closeWallpaperDrawer}>
           <div className="wallpaper-drawer">
@@ -66,7 +53,7 @@ class Header extends React.Component<WithStyles<StylesType>> {
             </LazilyLoad>
           </div>
         </Drawer>
-      </div>
+      </React.Fragment>
     )
   }
 }
