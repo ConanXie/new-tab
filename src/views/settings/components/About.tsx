cc46b0f434b75664d04b89e3099bcf3554607ed4
ConanXie <xjj416@gmail.com>
2018-09-25 00:09:59 +0800

    About info

--- src/views/settings/components/About.tsx
+++ src/views/settings/components/About.tsx
@@ -1,11 +1,139 @@
 import * as React from "react"
 
-class About extends React.Component {
+import { WithStyles, StyleRulesCallback, withStyles } from "@material-ui/core/styles"
+import Typography from "@material-ui/core/Typography"
+import IconButton from "@material-ui/core/IconButton"
+import Tooltip from "@material-ui/core/Tooltip"
+import ShareIcon from "@material-ui/icons/Share"
+import Snackbar from "@material-ui/core/Snackbar"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from "@material-ui/core/ListItemText"
+// import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction"
+
+import Wrap from "../Layout/SettingsWrap"
+import GitHubIcon from "./Icons/GitHub"
+import LogoIcon from "./Icons/Logo"
+
+const styles: StyleRulesCallback = theme => ({
+  sec: {
+    padding: "24px",
+  },
+  header: {
+    display: "flex",
+    alignItems: "center",
+    marginBottom: theme.spacing.unit,
+  },
+  logo: {
+    width: 40,
+    height: 40,
+    marginRight: theme.spacing.unit,
+    marginLeft: -theme.spacing.unit / 4,
+    color: theme.overrides!.MuiButton!.root!.color,
+  },
+  "@keyframes rotate": {
+    from: {
+      transform: "rotate(0deg)",
+    },
+    to: {
+      transform: "rotate(360deg)",
+    },
+  },
+  iconBtnWrap: {
+    textAlign: "right",
+    margin: -theme.spacing.unit * 2,
+  }
+})
+
+type StylesType = "sec"
+  | "header"
+  | "logo"
+  | "button"
+  | "iconBtnWrap"
+class About extends React.Component<WithStyles<StylesType>> {
+
+  public state = {
+    snackbarOpen: false,
+    message: "",
+  }
+
+  public readonly shared = "https://chrome.google.com/webstore/detail/kgfodmcknjlgkbgkkafogbdaibkfgdgo/"
+
+  public copyToClipboard = () => {
+    const input = document.createElement("input")
+    input.value = this.shared
+    document.body.appendChild(input)
+    input.focus()
+    input.setSelectionRange(0, this.shared.length)
+    document.execCommand("copy")
+    document.body.removeChild(input)
+
+    this.setState({
+      snackbarOpen: true,
+      message: chrome.i18n.getMessage("settings_about_clipboard"),
+    })
+  }
+
+  public handleSnackbarClose = () => {
+    this.setState({ snackbarOpen: false })
+  }
+
   public render() {
+    const { classes } = this.props
+    const { snackbarOpen, message } = this.state
     return (
-      <h1>About</h1>
+      <React.Fragment>
+        <Wrap>
+          <div className={classes.sec}>
+            <header className={classes.header}>
+              <LogoIcon className={classes.logo} />
+              <Typography variant="title">Material Design New Tab</Typography>
+            </header>
+            <Typography gutterBottom>Everyone's favorite extension</Typography>
+            <div className={classes.iconBtnWrap}>
+              <Tooltip title={chrome.i18n.getMessage("settings_about_share")}>
+                <IconButton onClick={this.copyToClipboard}>
+                  <ShareIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title={chrome.i18n.getMessage("settings_about_source_code")}>
+                <IconButton
+                  href="https://github.com/ConanXie/new-tab/"
+                  className={classes.button}
+                >
+                  <GitHubIcon />
+                </IconButton>
+              </Tooltip>
+            </div>
+          </div>
+        </Wrap>
+        <Wrap>
+          <List>
+            <ListItem
+              component="a"
+              button
+              href={
+                chrome.i18n.getUILanguage() === "zh-CN"
+                ? "https://tab.xiejie.co/logs"
+                : "https://github.com/ConanXie/new-tab/blob/master/CHANGELOG.md"
+              }
+            >
+              <ListItemText
+                primary={chrome.runtime.getManifest().version}
+                secondary="Click here to read the changelog"
+              />
+            </ListItem>
+          </List>
+        </Wrap>
+        <Snackbar
+          open={snackbarOpen}
+          message={message}
+          autoHideDuration={2000}
+          onClose={this.handleSnackbarClose}
+        />
+      </React.Fragment>
     )
   }
 }
 
-export default About
+export default withStyles(styles)(About)
