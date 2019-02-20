bd96ed7f0138fb0c8541af27a7dbc57625bb342d
ConanXie <xjj416@gmail.com>
2019-02-21 00:11:19 +0800

    Icons database

---
+++ src/views/background/icons.ts
@@ -0,0 +1,9 @@
+export default {
+  "bilibili.com": ["bilibili"],
+  "coolapk.com": ["coolapk"],
+  "www.google.com": ["google"],
+  "youtube.com": ["youtube"],
+  "facebook.com": ["facebook"],
+  // "twitter.com": ["twitter"],
+  "*": ["default"],
+}
