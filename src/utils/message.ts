3b6572ece96c406adcd3c6b080297e0ff4a9ff72
ConanXie <xjj416@gmail.com>
2019-02-21 00:10:29 +0800

    Fix type

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,10 +1,5 @@
-/**
- * send message
- * @param name message name
- * @param message message content
- * @param callback
- */
-export function sendMessage(name: string, message: any, callback?: (response?: any) => any) {
+/** Send message */
+export function sendMessage(name: string, message: any, callback?: (response: any) => any) {
   chrome.runtime.sendMessage({
     name,
     message
@@ -18,12 +13,12 @@ export function sendMessage(name: string, message: any, callback?: (response?: a
   })
 }
 
-/**
- * Message listener
- * @param name
- * @param callback
- */
-export function onMessage(name: string, callback?: (message?: any, sender?: any, sendResponse?: any) => any) {
+/** Add message listender */
+export function onMessage(name: string, callback?: (
+  message: any,
+  sender: chrome.runtime.MessageSender,
+  sendResponse: (response: any) => void
+) => any) {
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const { name: messageName, message: messageContent } = message
     if (messageName === name) {
