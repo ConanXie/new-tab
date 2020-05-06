5c02e7b044a728f7315ff9b2993bca2fb8838de7
ConanXie <xjj416@gmail.com>
2020-05-06 18:42:05 +0800

    add acrylic settings

---
+++ src/views/settings/components/Folders/WindowEffect.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+import { useLocalStore, observer } from "mobx-react"
+
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from "@material-ui/core/ListItemText"
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction"
+import Switch from "@material-ui/core/Switch"
+
+import SettingsTitle from "components/SettingsTitle"
+
+import { foldersSettings } from "../../store"
+
+function WindowEffect() {
+  const { acrylicEffect, toggleAcrylicEffect } = useLocalStore(() => foldersSettings)
+
+  return (
+    <>
+      <SettingsTitle>{chrome.i18n.getMessage("settings_effect")}</SettingsTitle>
+      <List>
+        <ListItem button onClick={toggleAcrylicEffect}>
+          <ListItemText
+            primary={chrome.i18n.getMessage("settings_folders_acrylic")}
+            secondary={chrome.i18n.getMessage("settings_folders_acrylic_secondary")}
+          />
+          <ListItemSecondaryAction>
+            <Switch color="primary" checked={acrylicEffect} onChange={toggleAcrylicEffect} />
+          </ListItemSecondaryAction>
+        </ListItem>
+      </List>
+    </>
+  )
+}
+
+export default observer(WindowEffect)
